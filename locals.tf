483f785e74f888befd94d43dd0520c9fcfbc4119
João Afonso Silva <joaoafonsodss@gmail.com>
2025-09-13 06:55:10 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -138,17 +138,17 @@ ENDCADDY
         ExecStart=/usr/bin/docker run --rm \
           --network custom-bridge \
           --name=actualtasks \
-          - e 'CRON_EXPRESSION'="${var.actual_tasks_config.cron_expression}" \
-          - e 'ACTUAL_SERVER_URL'="${var.actual_tasks_config.actual.server_url}" \
-          - e 'ACTUAL_SERVER_PASSWORD'="${var.actual_tasks_config.actual.server_password}" \
-          - e 'ACTUAL_SYNC_ID'="${var.actual_tasks_config.actual.sync_id}" \
-          - e 'ACTUAL_FILE_PASSWORD'="${var.actual_tasks_config.actual.file_password}" \
-          - e 'ENABLE_PAYEE_RENAME'=${var.actual_tasks_config.features.payee_rename.is_enabled} \
-          - e 'PAYEE_REGEX_MATCH'="${var.actual_tasks_config.features.payee_rename.regex_match}" \
-          - e 'ENABLE_INTEREST_CALCULATION'=${var.actual_tasks_config.features.interest_calculation.is_enabled} \
-          - e 'ENABLE_GHOSTFOLIO_SYNC'=${var.actual_tasks_config.features.ghostfolio.is_enabled} \
-          - e 'ENABLE_HOLD_INCOME_FOR_NEXT_MONTH'=${var.actual_tasks_config.features.hold_income_for_next_month.is_enabled} \
-          - e 'ENABLE_BANK_SYNC'=${var.actual_tasks_config.features.bank_sync.is_enabled} \
+          --env CRON_EXPRESSION="${var.actual_tasks_config.cron_expression}" \
+          --env ACTUAL_SERVER_URL="${var.actual_tasks_config.actual.server_url}" \
+          --env ACTUAL_SERVER_PASSWORD="${var.actual_tasks_config.actual.server_password}" \
+          --env ACTUAL_SYNC_ID="${var.actual_tasks_config.actual.sync_id}" \
+          --env ACTUAL_FILE_PASSWORD="${var.actual_tasks_config.actual.file_password}" \
+          --env ENABLE_PAYEE_RENAME=${var.actual_tasks_config.features.payee_rename.is_enabled} \
+          --env PAYEE_REGEX_MATCH="${var.actual_tasks_config.features.payee_rename.regex_match}" \
+          --env ENABLE_INTEREST_CALCULATION=${var.actual_tasks_config.features.interest_calculation.is_enabled} \
+          --env ENABLE_GHOSTFOLIO_SYNC=${var.actual_tasks_config.features.ghostfolio.is_enabled} \
+          --env ENABLE_HOLD_INCOME_FOR_NEXT_MONTH=${var.actual_tasks_config.features.hold_income_for_next_month.is_enabled} \
+          --env ENABLE_BANK_SYNC=${var.actual_tasks_config.features.bank_sync.is_enabled} \
           rodriguestiago0/actualtasks:${var.actual_tasks_image_version_tag}
 
         ExecStop=/usr/bin/docker stop actualtasks
