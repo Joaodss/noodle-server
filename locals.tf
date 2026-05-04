1ddac230a6db01738193e1a164b9a6639d422503
João Afonso Silva <joaoafonsodss@gmail.com>
2026-05-04 19:05:27 +0100

    update

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
   cloud_config = <<-EOT
-    #cloud-config
-    write_files:
+    write_files:      
       - path: /var/lib/cloud/scripts/per-boot/fs-prepare.sh
         permissions: "0544"
         owner: root
@@ -12,28 +11,24 @@ locals {
           MOUNT_DIR="/mnt/disks/data"
           PARENT_DIR="/mnt/disks"
           
-          # 1. PREVENT RAM OVERFLOW:
           mkdir -p $MOUNT_DIR
-          chmod 555 $PARENT_DIR  # Now Docker can't create "fake" folders here if sdb is missing
+          chmod 555 $PARENT_DIR
           
           while [ ! -b $DATA_DISK ]; do sleep 1; done
-
+          
           if ! blkid $DATA_DISK; then
             mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard $DATA_DISK
           fi
           
-          # 2. MOUNT SECURELY
           mount -o discard,defaults $DATA_DISK $MOUNT_DIR || true
           
-          # 3. DIRECTORY PREP
           if mountpoint -q $MOUNT_DIR; then
-             mkdir -p "$MOUNT_DIR/dockge" "$MOUNT_DIR/stacks" "$MOUNT_DIR/rclone-config"
              chown -R root:root $MOUNT_DIR
-             touch "$MOUNT_DIR/THIS_IS_THE_REAL_DISK"
           else
              echo "ERROR: Disk failed to mount! Preventing Docker writes."
              exit 1
           fi
+      
       - path: /etc/systemd/system/dockge.service
         permissions: "0644"
         owner: root
@@ -43,11 +38,11 @@ locals {
           After=network-online.target docker.service local-fs.target
           Requires=docker.service
           RequiresMountsFor=/mnt/disks/data
-
           [Service]
           Type=simple
           ExecStartPre=/usr/bin/docker pull louislam/dockge:latest
           ExecStart=/usr/bin/docker run --rm --name dockge \
+            --network custom-bridge \
             -v /var/run/docker.sock:/var/run/docker.sock \
             -v /mnt/disks/data/dockge:/app/data \
             -v /mnt/disks/data/stacks:/opt/stacks \
@@ -57,9 +52,6 @@ locals {
           ExecStop=/usr/bin/docker stop dockge
           Restart=always
           RestartSec=5
-
-          [Install]
-          WantedBy=multi-user.target
 
     runcmd:
       - bash /var/lib/cloud/scripts/per-boot/fs-prepare.sh
